2113775f7216764a1fa2c9edba85b70c70aa80f9
sub77 <sub77@ymail.com>
2015-01-22 19:37:47 +0100

    overlay

--- slim.mk
+++ slim.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_hybrid_wifionly.mk)
+$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
